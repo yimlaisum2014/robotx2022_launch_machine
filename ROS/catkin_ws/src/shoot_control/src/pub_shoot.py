52a2ec7f76fe8a6903551a5cd163283515991049
yimlaisum2014 <yimlaisum2014@example.com>
2022-11-15 07:49:21 +0800

    update dynamixel shooter code

--- ROS/catkin_ws/src/shoot_control/src/pub_shoot.py
+++ ROS/catkin_ws/src/shoot_control/src/pub_shoot.py
@@ -3,20 +3,44 @@
 import rospy
 from std_srvs.srv import Trigger, TriggerResponse
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Bool, Int16, Byte
+from std_msgs.msg import Bool, Int8, Byte
 
 
 class shotPub():
     def __init__(self):
-        self.pub = rospy.Publisher('/shoot_cmd', Byte, queue_size=1)
+        self.pub = rospy.Publisher('/wamv/shooting', Byte, queue_size=1)
+        self.pub_speed = rospy.Publisher('/shooter/speed', Int8, queue_size=1)
+        self.pub_step = rospy.Publisher('/shooter/step', Int8, queue_size=1)
+
         self.inital()
     
     def inital(self):
+    
         shoot = Byte
         shoot = 1
-        while not rospy.is_shutdown():
-            self.pub.publish(shoot)
 
+        speed = Int8
+        step = Int8
+
+        speed = 60
+        step = 10
+        self.pub_speed.publish(speed)
+        self.pub_step.publish(step)
+
+        while not rospy.is_shutdown():
+            # test~~~~
+            # self.pub.publish(shoot)
+            # rospy.sleep(2)
+            # print("shoot")
+            # test~~~~
+            for x in range(0, 3):
+                print(x)
+                self.pub.publish(shoot)
+                x += 1
+                rospy.sleep(3)
+            break
+
+                        
 if __name__=='__main__':
 
     rospy.init_node("shoot_pub", anonymous=False)
---
+++ ROS/catkin_ws/src/shoot_control/src/shooter_dyn.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_srvs.srv import Trigger, TriggerResponse
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Bool, Int16, Byte
+from dynamixel_sdk_examples.msg import SetPosition
+
+class shotPub():
+    def __init__(self):
+
+        self.sub_shoot = rospy.Subscriber('/wamv/shoot', Int16, self.shoot_cb)
+        self.pub_shoot = rospy.Publisher('/shooter/set_position',SetPosition, queue_size=1)
+
+        self.motor_home_position = 0
+        self.motor_id = 10
+
+        self.motor_step1 = 1100
+        self.motor_step2 = 2700
+        self.motor_step3 = 0
+
+        self.inital()
+
+        
+    def inital(self):
+        go_home = SetPosition()
+        go_home.id = self.motor_id
+        go_home.position =  self.motor_home_position 
+        self.pub_shoot.publish(go_home)
+        print("go home")
+    
+    def shoot_cb(self,msg):
+        shoot_num = Int16
+        shoot_num = msg.data
+
+        shoot_step1 = SetPosition()
+        shoot_step2 = SetPosition()
+        shoot_step3 = SetPosition()
+        
+        if (shoot_num == 1):
+            shoot_step1.id = self.motor_id 
+            shoot_step1.position = self.motor_step1
+            self.pub_shoot.publish(shoot_step1)
+            print("position 1")
+        
+        elif (shoot_num == 2):
+            shoot_step2.id = self.motor_id 
+            shoot_step2.position = self.motor_step2
+            self.pub_shoot.publish(shoot_step2)
+            print("position 2")
+        
+
+        elif (shoot_num == 3):
+            shoot_step3.id = self.motor_id 
+            shoot_step3.position = self.motor_step3
+            self.pub_shoot.publish(shoot_step3)
+            print("position 3")
+        
+        else :
+            print("out of range")
+
+
+        
+
+                        
+if __name__=='__main__':
+
+    rospy.init_node("shoot_pub", anonymous=False)
+    shoot = shotPub()
+
+    rospy.spin()
